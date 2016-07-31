b38dc47ab0770578bd6ef39151b3ed75a97802c6
Javier Lerones-Gallego <javier.lerones-gallego@capitalone.com>
2016-07-31 12:04:45 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Angular 1.5 Component Architecture App
+
+This is a sample skeleton app to show how to use the new Angular 1.5 component architecture where everything is a component. Including
+the main component ```<bees-knees></bees-knees>```. The app uses the new component router from Angular 2, compatible with 1.5.
+
+It uses ES6 as the programming language, SASS for styles, Webpack as the build tool, and eslint configured with the Airbnb rules.
+
+You might want to remove the ```no-console: 0``` rule from the ```.eslintrc``` file. I left it there because of all the ```console.log```
+statements in the home component to show the new lifecycle hooks that angular provides.
+
+Use this project as a quick way to start a new project from scratch, just ```git clone``` it, ```npm install```, and ```npm start```.
+
---
+++ src/app/app.js
@@ -0,0 +1,55 @@
+// Import the styles
+import '../index.scss';
+
+// Import angular
+import angular from 'angular';
+
+// Import uiRouter
+import uiRouter from 'angular-ui-router';
+
+// Import Angular config files
+import Config from './config';
+import Run from './run';
+
+// Import main App Component
+import { AppComponent } from './components/app/app.component';
+
+// Import Components
+import { HomeComponent } from './components/home/home.component';
+import { BoardComponent } from './components/board/board.component';
+import { TileComponent } from './components/tile/tile.component';
+import { GameComponent } from './components/game/game.component';
+
+// Import Services
+import GameService from './services/game.service';
+import StatsService from './services/stats.service';
+
+// Declare the angular app name
+const appName = 'com.javierlerones.minesweeper';
+
+// Set up the angular app object with its dependencies
+const app = angular.module(appName, [
+    uiRouter,
+]);
+
+// Configure the Angular App
+app.config(Config);
+app.run(Run);
+
+// Main app component
+app.component(AppComponent.name, AppComponent);
+
+// Components
+app.component(HomeComponent.name, HomeComponent);
+app.component(BoardComponent.name, BoardComponent);
+app.component(TileComponent.name, TileComponent);
+app.component(GameComponent.name, GameComponent);
+
+// Services
+app.service(GameService.name, GameService);
+app.service(StatsService.name, StatsService);
+
+// Bootstrap the Angular app when the document is ready
+angular.element(document).ready(() => {
+    angular.bootstrap(document, [appName]);
+});
