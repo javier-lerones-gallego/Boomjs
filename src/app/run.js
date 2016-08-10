34fc21d7eb862c35f73deea4d6094d3dad8dff0f
Javier Lerones-Gallego <javier.lerones-gallego@capitalone.com>
2016-08-10 10:50:11 -0700

    clears up gh-pages branch

--- src/app/run.js
+++
@@ -1,9 +0,0 @@
-
-/* ngInject */
-export default function Run($rootScope, $state) {
-    $rootScope.$on('$stateChangeError', (evt, to, toParams, from, fromParams, error) => {
-        if (error && error.error === 'game_not_found') {
-            $state.go('root.home');
-        }
-    });
-}
