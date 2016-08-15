d30ba517f675b381129d2b077efd6c43d89546ab
Javier Lerones-Gallego <javier.lerones-gallego@capitalone.com>
2016-08-15 16:26:30 -0700

    if going away from the game board the game pauses automatically, if going back into the game state and game is paused, the game resumes automatically

--- src/app/components/game/game.component.js
+++ src/app/components/game/game.component.js
@@ -1,6 +1,13 @@
 import template from './game.html';
 
 class GameController {
+    $onInit() {
+        // If the game is paused when entering the component, resume it.
+        if (this.game.isPaused) {
+            this.game.resume();
+        }
+    }
+
     get game() { return this.parent.game; }
 }
 
