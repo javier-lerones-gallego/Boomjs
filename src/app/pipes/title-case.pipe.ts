478dd9c5e16e1d8e33ffbf206dae8b2a7cb75d47
Javier Lerones-Gallego <javier.lerones-gallego@capitalone.com>
2016-09-06 22:53:01 -0700

    adds title case pipe

---
+++ src/app/pipes/title-case.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'titleCase'
+})
+export class TitleCasePipe implements PipeTransform {
+
+  transform(value: string, args?: any): string {
+    return value.replace(/\w\S*/g, (txt) =>
+            txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+  }
+
+}
